17dde4ced1b9ef8ee0be338b00812647751ccb0f
Yubinex <janhauck99@gmail.com>
2024-03-30 03:40:12 +0100

    outsource file access into a separate package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"github.com/yubinex/go-price-calculator/conversion"
+	"github.com/yubinex/go-price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -15,37 +14,19 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+	lines, err := filemanager.ReadLines("prices.txt")
 	if err != nil {
-		fmt.Println("Could not open file!")
 		fmt.Println(err)
-		return
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
-	if err != nil {
-		fmt.Println("Reading the file content failed!")
-		fmt.Println(err)
-		file.Close()
 		return
 	}
 
 	prices, err := conversion.StringsToFloats(lines)
 	if err != nil {
 		fmt.Println(err)
-		file.Close()
 		return
 	}
 
 	job.InputPrices = prices
-	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
