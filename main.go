9285605d8f67a775f3fa8acfb835a4cdd4cd0cb2
Yubinex <janhauck99@gmail.com>
2024-03-30 04:01:40 +0100

    implementing a FileManager struct

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
-import "github.com/yubinex/go-price-calculator/prices"
+import (
+	"fmt"
+
+	"github.com/yubinex/go-price-calculator/filemanager"
+	"github.com/yubinex/go-price-calculator/prices"
+)
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
 		priceJob.Process()
 	}
 }
